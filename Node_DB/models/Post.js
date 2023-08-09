20d3e4cfb413206ff75be785a14d0895dd53b961
Rhuan <86356608+RHU4N@users.noreply.github.com>
2023-08-09 08:20:20 -0300

    Add files via upload

---
+++ Node_DB/models/Post.js
@@ -0,0 +1,17 @@
+const db = require('./db');
+
+const Post = db.sequelize.define('postagens',{
+    titulo:{
+        type: db.Sequelize.STRING
+    },
+    conteudo:{
+        type: db.Sequelize.TEXT
+    },
+    img:{
+        type: db.Sequelize.STRING
+    }
+});
+
+Post.sync({force: true});
+
+module.exports = Post;
